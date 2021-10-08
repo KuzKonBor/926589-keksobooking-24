8f59339d320462652506a8ef991b4625b5a46c8e
KuzKonBor <82975463+KuzKonBor@users.noreply.github.com>
2021-10-08 20:12:28 +0300

    Задание №4 после коррекции

--- js/main.js
+++ js/main.js
@@ -16,34 +16,33 @@ const getRandomСoordinates = (minimumValue, maximumValue, afterTheComma) => {
 
 
 const getAvatar = () => {
-      const avatarId = getRandomNumber(1, 9)
-      let author = {
-      avatar:'img/avatars/user0' + avatarId + '.png',
-      };
-      return author;
-    };
+  const avatarId = getRandomNumber(1, 9);
+  let author = {
+    avatar: 'img/avatars/user0' + avatarId + '.png' };
+  return author;
+};
 
-  const type = [
-    'palace',
-    'flat',
-    'house',
-    'bungalow',
-    'hotel',
-  ];
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
 
-  const checkin = [
-    '12:00',
-    '13:00',
-    '14:00',
-  ];
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
 
-  const checkout = [
-    '12:00',
-    '13:00',
-    '14:00',
-  ];
+const CHECKOUTS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
 
-const features = [
+const FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -51,19 +50,19 @@ const features = [
   'elevator',
   'conditioner',
 ];
-const description = [
+const DESCRIPTIONS = [
   'big',
   'small',
   'average',
 ];
 
-const photos = [
+const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-const sumarOffersCount = 1;
+const SUMAR_OFFERS_COUNT = 1;
 
 const getRandomOfferElement = (elements) => {
   return elements[getRandomNumber(0, elements.length - 1)];
@@ -74,28 +73,27 @@ const createOffer = () => {
   const lon = getRandomСoordinates(139.70000, 139.80000, 5);
 
   return {
-    author:
-      (getAvatar)(),
+
+    author:getAvatar(),
 
 offer: {
-    title: 'Жилье в аренду',
-    address: lng +  ', ' + lon,
-    price: getRandomNumber(100, 1000),
-    type: getRandomOfferElement(type),
-    rooms: getRandomNumber(1, 50),
-    guests: getRandomNumber(1, 100),
-    checkin: getRandomOfferElement(checkin),
-    checkout: getRandomOfferElement(checkout),
-    features: getRandomOfferElement(features),
-    description: getRandomOfferElement(description),
-    photos: getRandomOfferElement(photos)
+  title: 'Жилье в аренду',
+  address: lng +  ', ' + lon,
+  price: getRandomNumber(100, 1000),
+  type: getRandomOfferElement(TYPES),
+  rooms: getRandomNumber(1, 50),
+  guests: getRandomNumber(1, 100),
+  checkin: getRandomOfferElement(CHECKINS),
+  checkout: getRandomOfferElement(CHECKOUTS),
+  features: getRandomOfferElement(FEATURES),
+  description: getRandomOfferElement(DESCRIPTIONS),
+  photos: getRandomOfferElement(PHOTOS),
     },
 
 location:{
       lng,
       lon,
-},
+}
 }
 };
-const similarOffers = Array.from({length: sumarOffersCount}, createOffer);
-console.log(similarOffers);
+const similarOffers = Array.from({length: SUMAR_OFFERS_COUNT}, createOffer);
