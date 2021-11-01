3adc04312437e9d62a5aa91bd769cb9f77abbd68
KuzKonBor <82975463+KuzKonBor@users.noreply.github.com>
2021-11-01 15:36:44 +0300

    Блокировка и разблокировка формы

---
+++ js/form-disabled.js
@@ -0,0 +1,28 @@
+import {adForm} from './form-guests-rooms.js';
+
+const mapFilters = document.querySelector('.map__filters');
+const mapFiltersSelects = mapFilters.querySelectorAll('select');
+const mapFiltersFieldsets = mapFilters.querySelectorAll('fieldset');
+const adFormFieldsets = adForm.querySelectorAll('fieldset');
+
+const adFormDisabled = () => {
+  adForm.classList.toggle('ad-form--disabled');
+  adFormFieldsets.forEach((fieldset) => {
+    fieldset.disabled = !fieldset.disabled;
+  });
+};
+
+const mapFiltersDisabled = () => {
+  mapFilters.classList.toggle('map__filters--disabled');
+  mapFiltersSelects.forEach((select) => {
+    select.disabled = !select.disabled;
+    mapFiltersFieldsets.disabled = !mapFiltersFieldsets.disabled;
+  });
+};
+
+const disableToggle = () => {
+  adFormDisabled();
+  mapFiltersDisabled();
+};
+
+disableToggle();
